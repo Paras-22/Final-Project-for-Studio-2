502a5540029ebc02795fddc720d2af99e4439cc1
Paras-22 <parasjaat22ji@gmail.com>
2024-10-27 02:59:29 +1300

    Add project files.

---
+++ Form3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// 
+// ==========================================
+// THis form is for Mazhar for Dashboard.
+// ==========================================
+//
+namespace Final_Project_for_Studio_2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
