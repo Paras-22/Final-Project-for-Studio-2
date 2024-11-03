7deca9c014f36d07f0a30f5ca17e722ef394be75
Paras-22 <parasjaat22ji@gmail.com>
2024-11-03 13:16:41 +1300

    Make some changes.

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -39,23 +39,7 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.labelCVC = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.labelDiscription2 = new System.Windows.Forms.Label();
-            this.labelDiscription1 = new System.Windows.Forms.Label();
-            this.labelTaxAmount = new System.Windows.Forms.Label();
-            this.labelTax = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.labelTotal = new System.Windows.Forms.Label();
-            this.labelPrice2 = new System.Windows.Forms.Label();
-            this.labelProduct2 = new System.Windows.Forms.Label();
-            this.labelPrice1 = new System.Windows.Forms.Label();
-            this.labelProduct1 = new System.Windows.Forms.Label();
-            this.labeltTotal = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.buttonCancel = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelPayment
@@ -159,186 +143,22 @@
             this.textBox6.Size = new System.Drawing.Size(166, 44);
             this.textBox6.TabIndex = 13;
             // 
-            // panel1
+            // button1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.panel2);
-            this.panel1.Controls.Add(this.labelDiscription2);
-            this.panel1.Controls.Add(this.labelDiscription1);
-            this.panel1.Controls.Add(this.labelTaxAmount);
-            this.panel1.Controls.Add(this.labelTax);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.labelTotal);
-            this.panel1.Controls.Add(this.labelPrice2);
-            this.panel1.Controls.Add(this.labelProduct2);
-            this.panel1.Controls.Add(this.labelPrice1);
-            this.panel1.Controls.Add(this.labelProduct1);
-            this.panel1.Controls.Add(this.labeltTotal);
-            this.panel1.Controls.Add(this.label7);
-            this.panel1.Location = new System.Drawing.Point(571, 106);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(355, 540);
-            this.panel1.TabIndex = 16;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.panel2.Location = new System.Drawing.Point(25, 407);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(300, 3);
-            this.panel2.TabIndex = 17;
-            // 
-            // labelDiscription2
-            // 
-            this.labelDiscription2.AutoSize = true;
-            this.labelDiscription2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDiscription2.Location = new System.Drawing.Point(51, 317);
-            this.labelDiscription2.Name = "labelDiscription2";
-            this.labelDiscription2.Size = new System.Drawing.Size(104, 22);
-            this.labelDiscription2.TabIndex = 23;
-            this.labelDiscription2.Text = "Discription2";
-            // 
-            // labelDiscription1
-            // 
-            this.labelDiscription1.AutoSize = true;
-            this.labelDiscription1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDiscription1.Location = new System.Drawing.Point(51, 191);
-            this.labelDiscription1.Name = "labelDiscription1";
-            this.labelDiscription1.Size = new System.Drawing.Size(104, 22);
-            this.labelDiscription1.TabIndex = 22;
-            this.labelDiscription1.Text = "Discription1";
-            // 
-            // labelTaxAmount
-            // 
-            this.labelTaxAmount.AutoSize = true;
-            this.labelTaxAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTaxAmount.Location = new System.Drawing.Point(206, 425);
-            this.labelTaxAmount.Name = "labelTaxAmount";
-            this.labelTaxAmount.Size = new System.Drawing.Size(119, 25);
-            this.labelTaxAmount.TabIndex = 21;
-            this.labelTaxAmount.Text = "Tax Amount";
-            // 
-            // labelTax
-            // 
-            this.labelTax.AutoSize = true;
-            this.labelTax.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTax.Location = new System.Drawing.Point(39, 427);
-            this.labelTax.Name = "labelTax";
-            this.labelTax.Size = new System.Drawing.Size(49, 25);
-            this.labelTax.TabIndex = 20;
-            this.labelTax.Text = "Tax";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(206, 475);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 25);
-            this.label1.TabIndex = 19;
-            this.label1.Text = "Amount";
-            // 
-            // labelTotal
-            // 
-            this.labelTotal.AutoSize = true;
-            this.labelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTotal.Location = new System.Drawing.Point(39, 475);
-            this.labelTotal.Name = "labelTotal";
-            this.labelTotal.Size = new System.Drawing.Size(67, 25);
-            this.labelTotal.TabIndex = 18;
-            this.labelTotal.Text = "Total ";
-            // 
-            // labelPrice2
-            // 
-            this.labelPrice2.AutoSize = true;
-            this.labelPrice2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelPrice2.Location = new System.Drawing.Point(218, 276);
-            this.labelPrice2.Name = "labelPrice2";
-            this.labelPrice2.Size = new System.Drawing.Size(61, 22);
-            this.labelPrice2.TabIndex = 17;
-            this.labelPrice2.Text = "Price2";
-            // 
-            // labelProduct2
-            // 
-            this.labelProduct2.AutoSize = true;
-            this.labelProduct2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelProduct2.Location = new System.Drawing.Point(51, 276);
-            this.labelProduct2.Name = "labelProduct2";
-            this.labelProduct2.Size = new System.Drawing.Size(82, 22);
-            this.labelProduct2.TabIndex = 16;
-            this.labelProduct2.Text = "Product2";
-            // 
-            // labelPrice1
-            // 
-            this.labelPrice1.AutoSize = true;
-            this.labelPrice1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelPrice1.Location = new System.Drawing.Point(218, 148);
-            this.labelPrice1.Name = "labelPrice1";
-            this.labelPrice1.Size = new System.Drawing.Size(61, 22);
-            this.labelPrice1.TabIndex = 15;
-            this.labelPrice1.Text = "Price1";
-            // 
-            // labelProduct1
-            // 
-            this.labelProduct1.AutoSize = true;
-            this.labelProduct1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelProduct1.Location = new System.Drawing.Point(51, 148);
-            this.labelProduct1.Name = "labelProduct1";
-            this.labelProduct1.Size = new System.Drawing.Size(82, 22);
-            this.labelProduct1.TabIndex = 14;
-            this.labelProduct1.Text = "Product1";
-            // 
-            // labeltTotal
-            // 
-            this.labeltTotal.AutoSize = true;
-            this.labeltTotal.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labeltTotal.Location = new System.Drawing.Point(98, 63);
-            this.labeltTotal.Name = "labeltTotal";
-            this.labeltTotal.Size = new System.Drawing.Size(75, 38);
-            this.labeltTotal.TabIndex = 13;
-            this.labeltTotal.Text = "Total";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(20, 21);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(129, 25);
-            this.label7.TabIndex = 12;
-            this.label7.Text = "You are paying";
-            // 
-            // buttonCancel
-            // 
-            this.buttonCancel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.buttonCancel.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonCancel.Location = new System.Drawing.Point(280, 593);
-            this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(169, 53);
-            this.buttonCancel.TabIndex = 18;
-            this.buttonCancel.Text = "Cancel";
-            this.buttonCancel.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(40, 593);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(169, 53);
-            this.button2.TabIndex = 17;
-            this.button2.Text = "Pay";
-            this.button2.UseVisualStyleBackColor = false;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(140, 593);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 53);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Pay";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1002, 712);
-            this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.labelCVC);
             this.Controls.Add(this.textBox3);
@@ -353,8 +173,6 @@
             this.Name = "Form4";
             this.Text = "PaymentPage";
             this.Load += new System.EventHandler(this.Form4_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -373,21 +191,6 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label labelCVC;
         private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label labelDiscription2;
-        private System.Windows.Forms.Label labelDiscription1;
-        private System.Windows.Forms.Label labelTaxAmount;
-        private System.Windows.Forms.Label labelTax;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label labelTotal;
-        private System.Windows.Forms.Label labelPrice2;
-        private System.Windows.Forms.Label labelProduct2;
-        private System.Windows.Forms.Label labelPrice1;
-        private System.Windows.Forms.Label labelProduct1;
-        private System.Windows.Forms.Label labeltTotal;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button buttonCancel;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
