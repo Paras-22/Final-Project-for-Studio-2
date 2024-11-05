f475e2c8d7523064ab2ca3dc239bce0afe2a9c45
mazhar1341 <faizimazhar2@gmail.com>
2024-11-06 10:24:03 +1300

    I added the code to the mainfolder of final project (Paras)

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form3
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,28 +13,17 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.SuspendLayout();
             // 
-            // Form3
+            // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1002, 712);
-            this.Name = "Form3";
-            this.Text = "DashboardPage";
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
 
         }
-
-        #endregion
     }
 }
