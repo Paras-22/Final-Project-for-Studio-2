e6df4ca3e7776a8e5d85a50a92adc9366ad2d394
Roshan Aryal <roshanaryaal@gmail.com>
2024-11-08 11:41:09 +1300

    changes on checkout time and colour

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -29,60 +29,101 @@
             this.btnCheckout = new System.Windows.Forms.Button();
             this.lblTrolleyStatus = new System.Windows.Forms.Label();
             this.lblSubtotal = new System.Windows.Forms.Label();
+            this.lblDeliveryFee = new System.Windows.Forms.Label();
+            this.lblEstimatedTotal = new System.Windows.Forms.Label();
+            this.lblCountdown = new System.Windows.Forms.Label();
             this.SuspendLayout();
+
             // 
             // btnContinueShopping
             // 
-            this.btnContinueShopping.Location = new System.Drawing.Point(50, 250);
+            this.btnContinueShopping.Location = new System.Drawing.Point(50, 400);
             this.btnContinueShopping.Name = "btnContinueShopping";
-            this.btnContinueShopping.Size = new System.Drawing.Size(150, 40);
+            this.btnContinueShopping.Size = new System.Drawing.Size(180, 50);
             this.btnContinueShopping.TabIndex = 0;
             this.btnContinueShopping.Text = "Continue Shopping";
             this.btnContinueShopping.UseVisualStyleBackColor = true;
             this.btnContinueShopping.Click += new System.EventHandler(this.btnContinueShopping_Click);
+            this.btnContinueShopping.BackColor = System.Drawing.Color.FromArgb(0, 123, 0); // Green
+
             // 
             // btnClearTrolley
             // 
-            this.btnClearTrolley.Location = new System.Drawing.Point(50, 300);
+            this.btnClearTrolley.Location = new System.Drawing.Point(250, 400);
             this.btnClearTrolley.Name = "btnClearTrolley";
-            this.btnClearTrolley.Size = new System.Drawing.Size(150, 40);
+            this.btnClearTrolley.Size = new System.Drawing.Size(180, 50);
             this.btnClearTrolley.TabIndex = 1;
             this.btnClearTrolley.Text = "Clear Trolley";
             this.btnClearTrolley.UseVisualStyleBackColor = true;
             this.btnClearTrolley.Click += new System.EventHandler(this.btnClearTrolley_Click);
+            this.btnClearTrolley.BackColor = System.Drawing.Color.FromArgb(192, 0, 0); // Red
+
             // 
             // btnCheckout
             // 
-            this.btnCheckout.Location = new System.Drawing.Point(50, 350);
+            this.btnCheckout.Location = new System.Drawing.Point(450, 400);
             this.btnCheckout.Name = "btnCheckout";
-            this.btnCheckout.Size = new System.Drawing.Size(150, 40);
+            this.btnCheckout.Size = new System.Drawing.Size(180, 50);
             this.btnCheckout.TabIndex = 2;
             this.btnCheckout.Text = "Checkout";
             this.btnCheckout.UseVisualStyleBackColor = true;
             this.btnCheckout.Click += new System.EventHandler(this.btnCheckout_Click);
+            this.btnCheckout.BackColor = System.Drawing.Color.FromArgb(0, 102, 204); // Blue
+
             // 
             // lblTrolleyStatus
             // 
             this.lblTrolleyStatus.AutoSize = true;
-            this.lblTrolleyStatus.Location = new System.Drawing.Point(50, 50);
+            this.lblTrolleyStatus.Location = new System.Drawing.Point(50, 30);
             this.lblTrolleyStatus.Name = "lblTrolleyStatus";
-            this.lblTrolleyStatus.Size = new System.Drawing.Size(109, 20);
-            this.lblTrolleyStatus.TabIndex = 3;
-            this.lblTrolleyStatus.Text = "Trolley Status:";
+            this.lblTrolleyStatus.Size = new System.Drawing.Size(100, 20);
+            this.lblTrolleyStatus.Text = "Trolley: No items";
+
+            // 
+            // lblDeliveryFee
+            // 
+            this.lblDeliveryFee.AutoSize = true;
+            this.lblDeliveryFee.Location = new System.Drawing.Point(50, 80);
+            this.lblDeliveryFee.Name = "lblDeliveryFee";
+            this.lblDeliveryFee.Size = new System.Drawing.Size(100, 20);
+            this.lblDeliveryFee.Text = "Delivery Fee: $14.00";
+
             // 
             // lblSubtotal
             // 
             this.lblSubtotal.AutoSize = true;
-            this.lblSubtotal.Location = new System.Drawing.Point(50, 100);
+            this.lblSubtotal.Location = new System.Drawing.Point(50, 120);
             this.lblSubtotal.Name = "lblSubtotal";
-            this.lblSubtotal.Size = new System.Drawing.Size(77, 20);
-            this.lblSubtotal.TabIndex = 4;
-            this.lblSubtotal.Text = "Subtotal: ";
+            this.lblSubtotal.Size = new System.Drawing.Size(100, 20);
+            this.lblSubtotal.Text = "Subtotal: $0.00";
+
+            // 
+            // lblEstimatedTotal
+            // 
+            this.lblEstimatedTotal.AutoSize = true;
+            this.lblEstimatedTotal.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
+            this.lblEstimatedTotal.Location = new System.Drawing.Point(50, 160);
+            this.lblEstimatedTotal.Name = "lblEstimatedTotal";
+            this.lblEstimatedTotal.Size = new System.Drawing.Size(180, 30);
+            this.lblEstimatedTotal.Text = "Estimated Total: $14.00";
+
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Location = new System.Drawing.Point(50, 200);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(120, 20);
+            this.lblCountdown.Text = "Time Left: 60 seconds";
+
             // 
             // Form5
             // 
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 500);
+            this.Controls.Add(this.lblCountdown);
+            this.Controls.Add(this.lblEstimatedTotal);
             this.Controls.Add(this.lblSubtotal);
+            this.Controls.Add(this.lblDeliveryFee);
             this.Controls.Add(this.lblTrolleyStatus);
             this.Controls.Add(this.btnCheckout);
             this.Controls.Add(this.btnClearTrolley);
@@ -92,7 +133,6 @@
             this.Load += new System.EventHandler(this.Form5_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
@@ -102,5 +142,8 @@
         private System.Windows.Forms.Button btnCheckout;
         private System.Windows.Forms.Label lblTrolleyStatus;
         private System.Windows.Forms.Label lblSubtotal;
+        private System.Windows.Forms.Label lblDeliveryFee;
+        private System.Windows.Forms.Label lblEstimatedTotal;
+        private System.Windows.Forms.Label lblCountdown;
     }
 }
--- Form5.cs
+++ Form5.cs
@@ -5,32 +5,67 @@ namespace Final_Project_for_Studio_2
 {
     public partial class Form5 : Form
     {
+        // Declare a timer for countdown functionality
+        private Timer countdownTimer;
+        private int countdownTime = 60; // 60 seconds countdown
+
+        // Constructor for Form5
         public Form5()
         {
             InitializeComponent();
+            InitializeCountdownTimer();
         }
 
+        // Initialize countdown timer
+        private void InitializeCountdownTimer()
+        {
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 1000; // 1 second interval
+            countdownTimer.Tick += CountdownTimer_Tick;
+            lblCountdown.Text = $"Time Left: {countdownTime} seconds";
+        }
+
+        // Countdown timer tick event
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            countdownTime--;
+            lblCountdown.Text = $"Time Left: {countdownTime} seconds";
+
+            if (countdownTime <= 0)
+            {
+                countdownTimer.Stop();
+                MessageBox.Show("Checkout time has expired. Please try again.");
+            }
+        }
+
+        // Button click event for Continue Shopping
         private void btnContinueShopping_Click(object sender, EventArgs e)
         {
-            // Code for continuing shopping
-            MessageBox.Show("Continue shopping functionality goes here!");
+            MessageBox.Show("Continuing shopping...");
         }
 
+        // Button click event for Clear Trolley
         private void btnClearTrolley_Click(object sender, EventArgs e)
         {
-            // Code to clear the trolley
-            MessageBox.Show("Clear trolley functionality goes here!");
+            MessageBox.Show("Trolley cleared.");
+            // Clear the trolley and reset the subtotal and estimated total values
+            lblTrolleyStatus.Text = "Trolley: No items";
+            lblSubtotal.Text = "Subtotal: $0.00";
+            lblEstimatedTotal.Text = "Estimated Total: $0.00";
         }
 
+        // Button click event for Checkout
         private void btnCheckout_Click(object sender, EventArgs e)
         {
-            // Code for checkout functionality
-            MessageBox.Show("Checkout functionality goes here!");
+            countdownTimer.Stop();
+            MessageBox.Show("Proceeding to checkout...");
+            // Add any additional checkout logic here if necessary
         }
 
+        // Form load event to start countdown timer
         private void Form5_Load(object sender, EventArgs e)
         {
-
+            countdownTimer.Start();
         }
     }
 }
