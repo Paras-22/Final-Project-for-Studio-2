f8d016a8db90dc99af9f4da74d32fa88073a14b1
Roshan Aryal <roshanaryaal@gmail.com>
2024-11-11 15:28:43 +1300

    Enhanced some code and added some more feature on pervious ones

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -1,5 +1,3 @@
-using System.Security.Policy;
-
 namespace Final_Project_for_Studio_2
 {
     partial class Form5
@@ -23,6 +21,7 @@ namespace Final_Project_for_Studio_2
             this.btnClearTrolley = new System.Windows.Forms.Button();
             this.btnCheckout = new System.Windows.Forms.Button();
             this.lblTrolleyStatus = new System.Windows.Forms.Label();
+            this.lstItems = new System.Windows.Forms.ListBox();
             this.lblSubtotal = new System.Windows.Forms.Label();
             this.lblDeliveryFee = new System.Windows.Forms.Label();
             this.lblEstimatedTotal = new System.Windows.Forms.Label();
@@ -40,9 +39,9 @@ namespace Final_Project_for_Studio_2
             this.btnContinueShopping.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(123)))), ((int)(((byte)(0)))));
             this.btnContinueShopping.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.btnContinueShopping.ForeColor = System.Drawing.Color.White;
-            this.btnContinueShopping.Location = new System.Drawing.Point(20, 20);
+            this.btnContinueShopping.Location = new System.Drawing.Point(50, 20);
             this.btnContinueShopping.Name = "btnContinueShopping";
-            this.btnContinueShopping.Size = new System.Drawing.Size(160, 50);
+            this.btnContinueShopping.Size = new System.Drawing.Size(200, 50);
             this.btnContinueShopping.TabIndex = 0;
             this.btnContinueShopping.Text = "Continue Shopping";
             this.btnContinueShopping.UseVisualStyleBackColor = false;
@@ -53,9 +52,9 @@ namespace Final_Project_for_Studio_2
             this.btnClearTrolley.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.btnClearTrolley.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.btnClearTrolley.ForeColor = System.Drawing.Color.White;
-            this.btnClearTrolley.Location = new System.Drawing.Point(220, 20);
+            this.btnClearTrolley.Location = new System.Drawing.Point(280, 20);
             this.btnClearTrolley.Name = "btnClearTrolley";
-            this.btnClearTrolley.Size = new System.Drawing.Size(160, 50);
+            this.btnClearTrolley.Size = new System.Drawing.Size(200, 50);
             this.btnClearTrolley.TabIndex = 1;
             this.btnClearTrolley.Text = "Clear Trolley";
             this.btnClearTrolley.UseVisualStyleBackColor = false;
@@ -66,9 +65,9 @@ namespace Final_Project_for_Studio_2
             this.btnCheckout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.btnCheckout.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.btnCheckout.ForeColor = System.Drawing.Color.White;
-            this.btnCheckout.Location = new System.Drawing.Point(420, 20);
+            this.btnCheckout.Location = new System.Drawing.Point(510, 20);
             this.btnCheckout.Name = "btnCheckout";
-            this.btnCheckout.Size = new System.Drawing.Size(160, 50);
+            this.btnCheckout.Size = new System.Drawing.Size(200, 50);
             this.btnCheckout.TabIndex = 2;
             this.btnCheckout.Text = "Checkout";
             this.btnCheckout.UseVisualStyleBackColor = false;
@@ -77,22 +76,33 @@ namespace Final_Project_for_Studio_2
             // lblTrolleyStatus
             // 
             this.lblTrolleyStatus.AutoSize = true;
-            this.lblTrolleyStatus.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTrolleyStatus.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
             this.lblTrolleyStatus.Location = new System.Drawing.Point(20, 20);
             this.lblTrolleyStatus.Name = "lblTrolleyStatus";
-            this.lblTrolleyStatus.Size = new System.Drawing.Size(244, 33);
+            this.lblTrolleyStatus.Size = new System.Drawing.Size(224, 37);
             this.lblTrolleyStatus.TabIndex = 0;
-            this.lblTrolleyStatus.Text = "Trolley: No items";
+            this.lblTrolleyStatus.Text = "Trolley Items:";
+            // 
+            // lstItems
+            // 
+            this.lstItems.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstItems.Font = new System.Drawing.Font("Arial", 10F);
+            this.lstItems.FormattingEnabled = true;
+            this.lstItems.ItemHeight = 23;
+            this.lstItems.Location = new System.Drawing.Point(20, 60);
+            this.lstItems.Name = "lstItems";
+            this.lstItems.Size = new System.Drawing.Size(300, 140);
+            this.lstItems.TabIndex = 1;
             // 
             // lblSubtotal
             // 
             this.lblSubtotal.AutoSize = true;
             this.lblSubtotal.Font = new System.Drawing.Font("Arial", 12F);
-            this.lblSubtotal.Location = new System.Drawing.Point(3, 50);
+            this.lblSubtotal.Location = new System.Drawing.Point(3, 20);
             this.lblSubtotal.Name = "lblSubtotal";
-            this.lblSubtotal.Size = new System.Drawing.Size(109, 27);
+            this.lblSubtotal.Size = new System.Drawing.Size(175, 20);
             this.lblSubtotal.TabIndex = 1;
-            this.lblSubtotal.Text = "Subtotal:";
+            this.lblSubtotal.Text = "Subtotal: $0.00";
             // 
             // lblDeliveryFee
             // 
@@ -100,30 +110,30 @@ namespace Final_Project_for_Studio_2
             this.lblDeliveryFee.Font = new System.Drawing.Font("Arial", 12F);
             this.lblDeliveryFee.Location = new System.Drawing.Point(3, 0);
             this.lblDeliveryFee.Name = "lblDeliveryFee";
-            this.lblDeliveryFee.Size = new System.Drawing.Size(152, 27);
+            this.lblDeliveryFee.Size = new System.Drawing.Size(218, 20);
             this.lblDeliveryFee.TabIndex = 0;
-            this.lblDeliveryFee.Text = "Delivery Fee:";
+            this.lblDeliveryFee.Text = "Delivery Fee: $0.00";
             // 
             // lblEstimatedTotal
             // 
             this.lblEstimatedTotal.AutoSize = true;
-            this.lblEstimatedTotal.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
+            this.lblEstimatedTotal.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
             this.lblEstimatedTotal.ForeColor = System.Drawing.Color.DarkGreen;
-            this.lblEstimatedTotal.Location = new System.Drawing.Point(3, 100);
+            this.lblEstimatedTotal.Location = new System.Drawing.Point(3, 40);
             this.lblEstimatedTotal.Name = "lblEstimatedTotal";
-            this.lblEstimatedTotal.Size = new System.Drawing.Size(374, 37);
+            this.lblEstimatedTotal.Size = new System.Drawing.Size(171, 66);
             this.lblEstimatedTotal.TabIndex = 2;
-            this.lblEstimatedTotal.Text = "Estimated Total: $14.00";
+            this.lblEstimatedTotal.Text = "Estimated Total: $0.00";
             // 
             // lblCountdown
             // 
             this.lblCountdown.AutoSize = true;
-            this.lblCountdown.Font = new System.Drawing.Font("Arial", 10F);
+            this.lblCountdown.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Italic);
             this.lblCountdown.ForeColor = System.Drawing.Color.Red;
             this.lblCountdown.Location = new System.Drawing.Point(20, 240);
             this.lblCountdown.Name = "lblCountdown";
-            this.lblCountdown.Size = new System.Drawing.Size(205, 23);
-            this.lblCountdown.TabIndex = 1;
+            this.lblCountdown.Size = new System.Drawing.Size(208, 24);
+            this.lblCountdown.TabIndex = 2;
             this.lblCountdown.Text = "Time Left: 60 seconds";
             // 
             // pnlTrolleyInfo
@@ -131,13 +141,13 @@ namespace Final_Project_for_Studio_2
             this.pnlTrolleyInfo.BackColor = System.Drawing.Color.WhiteSmoke;
             this.pnlTrolleyInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pnlTrolleyInfo.Controls.Add(this.lblTrolleyStatus);
+            this.pnlTrolleyInfo.Controls.Add(this.lstItems);
             this.pnlTrolleyInfo.Controls.Add(this.lblCountdown);
             this.pnlTrolleyInfo.Controls.Add(this.tblLayoutPrices);
             this.pnlTrolleyInfo.Location = new System.Drawing.Point(50, 30);
             this.pnlTrolleyInfo.Name = "pnlTrolleyInfo";
-            this.pnlTrolleyInfo.Size = new System.Drawing.Size(853, 346);
+            this.pnlTrolleyInfo.Size = new System.Drawing.Size(700, 300);
             this.pnlTrolleyInfo.TabIndex = 0;
-            this.pnlTrolleyInfo.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlTrolleyInfo_Paint);
             // 
             // tblLayoutPrices
             // 
@@ -147,14 +157,14 @@ namespace Final_Project_for_Studio_2
             this.tblLayoutPrices.Controls.Add(this.lblDeliveryFee, 0, 0);
             this.tblLayoutPrices.Controls.Add(this.lblSubtotal, 0, 1);
             this.tblLayoutPrices.Controls.Add(this.lblEstimatedTotal, 0, 2);
-            this.tblLayoutPrices.Location = new System.Drawing.Point(20, 60);
+            this.tblLayoutPrices.Location = new System.Drawing.Point(350, 60);
             this.tblLayoutPrices.Name = "tblLayoutPrices";
             this.tblLayoutPrices.RowCount = 3;
-            this.tblLayoutPrices.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
-            this.tblLayoutPrices.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
-            this.tblLayoutPrices.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
-            this.tblLayoutPrices.Size = new System.Drawing.Size(560, 150);
-            this.tblLayoutPrices.TabIndex = 2;
+            this.tblLayoutPrices.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblLayoutPrices.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblLayoutPrices.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblLayoutPrices.Size = new System.Drawing.Size(330, 150);
+            this.tblLayoutPrices.TabIndex = 3;
             // 
             // pnlActions
             // 
@@ -162,15 +172,15 @@ namespace Final_Project_for_Studio_2
             this.pnlActions.Controls.Add(this.btnContinueShopping);
             this.pnlActions.Controls.Add(this.btnClearTrolley);
             this.pnlActions.Controls.Add(this.btnCheckout);
-            this.pnlActions.Location = new System.Drawing.Point(50, 417);
+            this.pnlActions.Location = new System.Drawing.Point(50, 360);
             this.pnlActions.Name = "pnlActions";
-            this.pnlActions.Size = new System.Drawing.Size(853, 191);
+            this.pnlActions.Size = new System.Drawing.Size(700, 100);
             this.pnlActions.TabIndex = 1;
-            this.pnlActions.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlActions_Paint);
             // 
             // Form5
             // 
-            this.ClientSize = new System.Drawing.Size(1002, 712);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 550);
             this.Controls.Add(this.pnlTrolleyInfo);
             this.Controls.Add(this.pnlActions);
             this.Name = "Form5";
@@ -191,6 +201,7 @@ namespace Final_Project_for_Studio_2
         private System.Windows.Forms.Button btnClearTrolley;
         private System.Windows.Forms.Button btnCheckout;
         private System.Windows.Forms.Label lblTrolleyStatus;
+        private System.Windows.Forms.ListBox lstItems;
         private System.Windows.Forms.Label lblSubtotal;
         private System.Windows.Forms.Label lblDeliveryFee;
         private System.Windows.Forms.Label lblEstimatedTotal;
