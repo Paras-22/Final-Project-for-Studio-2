5bb55cb3e73987b8fb2d9bf73eea393c6b31d4c8
Roshan Aryal <roshanaryaal@gmail.com>
2024-11-20 13:35:58 +1300

    added new degine where we can upload photos and rate 2 types service and product

--- Feedback.Designer.cs
+++ Feedback.Designer.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace Final_Project_for_Studio_2
 {
@@ -10,20 +11,27 @@ namespace Final_Project_for_Studio_2
         private Label lblHeader;
         private Label lblSubHeader;
         private GroupBox grpFeedbackForm;
+        private Label lblOrderNumber;
+        private TextBox txtOrderNumber;
         private Label lblName;
         private TextBox txtName;
         private Label lblEmail;
         private TextBox txtEmail;
-        private Label lblRating;
-        private ComboBox cmbRating;
+        private Label lblProductRating;
+        private ComboBox cmbProductRating;
+        private Label lblServiceRating;
+        private ComboBox cmbServiceRating;
+        private Label lblFeedbackType;
+        private ComboBox cmbFeedbackType;
         private Label lblComments;
         private TextBox txtComments;
         private Button btnSubmit;
+        private Button btnReset;
+        private Label lblImageUpload;
+        private Button btnUploadImage;
+        private Label lblImageStatus;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        // Dispose method to clean up resources
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -33,168 +41,135 @@ namespace Final_Project_for_Studio_2
             base.Dispose(disposing);
         }
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        // Form initialization method
         private void InitializeComponent()
         {
-            this.lblHeader = new System.Windows.Forms.Label();
-            this.lblSubHeader = new System.Windows.Forms.Label();
-            this.grpFeedbackForm = new System.Windows.Forms.GroupBox();
-            this.lblName = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.lblEmail = new System.Windows.Forms.Label();
-            this.txtEmail = new System.Windows.Forms.TextBox();
-            this.lblRating = new System.Windows.Forms.Label();
-            this.cmbRating = new System.Windows.Forms.ComboBox();
-            this.lblComments = new System.Windows.Forms.Label();
-            this.txtComments = new System.Windows.Forms.TextBox();
-            this.btnSubmit = new System.Windows.Forms.Button();
-            this.grpFeedbackForm.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // lblHeader
-            // 
-            this.lblHeader.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold);
-            this.lblHeader.ForeColor = System.Drawing.Color.Black;
-            this.lblHeader.Location = new System.Drawing.Point(362, 20);
-            this.lblHeader.Name = "lblHeader";
-            this.lblHeader.Size = new System.Drawing.Size(300, 55);
-            this.lblHeader.TabIndex = 0;
-            this.lblHeader.Text = "Feedback Form";
-            this.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblSubHeader
-            // 
-            this.lblSubHeader.Font = new System.Drawing.Font("Arial", 12F);
-            this.lblSubHeader.ForeColor = System.Drawing.Color.Gray;
-            this.lblSubHeader.Location = new System.Drawing.Point(119, 75);
-            this.lblSubHeader.Name = "lblSubHeader";
-            this.lblSubHeader.Size = new System.Drawing.Size(762, 30);
-            this.lblSubHeader.TabIndex = 1;
-            this.lblSubHeader.Text = "We appreciate your feedback. Please let us know your thoughts!";
-            this.lblSubHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblSubHeader.Click += new System.EventHandler(this.lblSubHeader_Click);
-            // 
-            // grpFeedbackForm
-            // 
+            // Form Properties
+            this.ClientSize = new System.Drawing.Size(1024, 768); // Set the form size
+            this.Text = "Feedback - TrendMart"; // Form Title
+
+            // Header Label
+            this.lblHeader = new Label
+            {
+                Text = "Feedback Form",
+                Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold),
+                ForeColor = System.Drawing.Color.Black,
+                Location = new System.Drawing.Point(362, 20),
+                Size = new System.Drawing.Size(300, 40),
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+            };
+
+            // Subheader Label
+            this.lblSubHeader = new Label
+            {
+                Text = "We value your feedback. Please let us know about your experience!",
+                Font = new System.Drawing.Font("Arial", 12F),
+                ForeColor = System.Drawing.Color.Gray,
+                Location = new System.Drawing.Point(250, 70),
+                Size = new System.Drawing.Size(500, 30),
+                TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+            };
+
+            // Feedback Form GroupBox
+            this.grpFeedbackForm = new GroupBox
+            {
+                Text = "Your Feedback",
+                Location = new System.Drawing.Point(50, 120),
+                Size = new System.Drawing.Size(900, 500),
+            };
+
+            // Controls Initialization
+            this.InitializeFormControls();
+
+            // Add Controls to GroupBox
+            this.grpFeedbackForm.Controls.Add(this.lblOrderNumber);
+            this.grpFeedbackForm.Controls.Add(this.txtOrderNumber);
             this.grpFeedbackForm.Controls.Add(this.lblName);
             this.grpFeedbackForm.Controls.Add(this.txtName);
             this.grpFeedbackForm.Controls.Add(this.lblEmail);
             this.grpFeedbackForm.Controls.Add(this.txtEmail);
-            this.grpFeedbackForm.Controls.Add(this.lblRating);
-            this.grpFeedbackForm.Controls.Add(this.cmbRating);
+            this.grpFeedbackForm.Controls.Add(this.lblProductRating);
+            this.grpFeedbackForm.Controls.Add(this.cmbProductRating);
+            this.grpFeedbackForm.Controls.Add(this.lblServiceRating);
+            this.grpFeedbackForm.Controls.Add(this.cmbServiceRating);
+            this.grpFeedbackForm.Controls.Add(this.lblFeedbackType);
+            this.grpFeedbackForm.Controls.Add(this.cmbFeedbackType);
             this.grpFeedbackForm.Controls.Add(this.lblComments);
             this.grpFeedbackForm.Controls.Add(this.txtComments);
             this.grpFeedbackForm.Controls.Add(this.btnSubmit);
-            this.grpFeedbackForm.Location = new System.Drawing.Point(50, 120);
-            this.grpFeedbackForm.Name = "grpFeedbackForm";
-            this.grpFeedbackForm.Size = new System.Drawing.Size(900, 400);
-            this.grpFeedbackForm.TabIndex = 2;
-            this.grpFeedbackForm.TabStop = false;
-            this.grpFeedbackForm.Text = "Your Feedback";
-            // 
-            // lblName
-            // 
-            this.lblName.Location = new System.Drawing.Point(20, 30);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(100, 25);
-            this.lblName.TabIndex = 0;
-            this.lblName.Text = "Your Name:";
-            // 
-            // txtName
-            // 
-            this.txtName.Font = new System.Drawing.Font("Arial", 10F);
-            this.txtName.Location = new System.Drawing.Point(120, 30);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(300, 30);
-            this.txtName.TabIndex = 1;
-            // 
-            // lblEmail
-            // 
-            this.lblEmail.Location = new System.Drawing.Point(20, 80);
-            this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(100, 25);
-            this.lblEmail.TabIndex = 2;
-            this.lblEmail.Text = "Email Address:";
-            // 
-            // txtEmail
-            // 
-            this.txtEmail.Font = new System.Drawing.Font("Arial", 10F);
-            this.txtEmail.Location = new System.Drawing.Point(120, 80);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(300, 30);
-            this.txtEmail.TabIndex = 3;
-            // 
-            // lblRating
-            // 
-            this.lblRating.Location = new System.Drawing.Point(20, 130);
-            this.lblRating.Name = "lblRating";
-            this.lblRating.Size = new System.Drawing.Size(100, 25);
-            this.lblRating.TabIndex = 4;
-            this.lblRating.Text = "Rating (1-5):";
-            // 
-            // cmbRating
-            // 
-            this.cmbRating.Font = new System.Drawing.Font("Arial", 10F);
-            this.cmbRating.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"});
-            this.cmbRating.Location = new System.Drawing.Point(120, 130);
-            this.cmbRating.Name = "cmbRating";
-            this.cmbRating.Size = new System.Drawing.Size(100, 31);
-            this.cmbRating.TabIndex = 5;
-            // 
-            // lblComments
-            // 
-            this.lblComments.Location = new System.Drawing.Point(20, 180);
-            this.lblComments.Name = "lblComments";
-            this.lblComments.Size = new System.Drawing.Size(100, 25);
-            this.lblComments.TabIndex = 6;
-            this.lblComments.Text = "Comments:";
-            // 
-            // txtComments
-            // 
-            this.txtComments.Font = new System.Drawing.Font("Arial", 10F);
-            this.txtComments.Location = new System.Drawing.Point(120, 180);
-            this.txtComments.Multiline = true;
-            this.txtComments.Name = "txtComments";
-            this.txtComments.Size = new System.Drawing.Size(600, 80);
-            this.txtComments.TabIndex = 7;
-            // 
-            // btnSubmit
-            // 
-            this.btnSubmit.BackColor = System.Drawing.Color.Green;
-            this.btnSubmit.FlatAppearance.BorderSize = 0;
-            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSubmit.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
-            this.btnSubmit.ForeColor = System.Drawing.Color.White;
-            this.btnSubmit.Location = new System.Drawing.Point(120, 280);
-            this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(200, 40);
-            this.btnSubmit.TabIndex = 8;
-            this.btnSubmit.Text = "Submit Feedback";
-            this.btnSubmit.UseVisualStyleBackColor = false;
-            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
-            // 
-            // Feedback
-            // 
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.grpFeedbackForm.Controls.Add(this.btnReset);
+            this.grpFeedbackForm.Controls.Add(this.lblImageUpload);
+            this.grpFeedbackForm.Controls.Add(this.btnUploadImage);
+            this.grpFeedbackForm.Controls.Add(this.lblImageStatus);
+
+            // Add GroupBox and other Controls to Form
             this.Controls.Add(this.lblHeader);
             this.Controls.Add(this.lblSubHeader);
             this.Controls.Add(this.grpFeedbackForm);
-            this.Name = "Feedback";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "TrendMart - Customer Feedback";
-            this.grpFeedbackForm.ResumeLayout(false);
-            this.grpFeedbackForm.PerformLayout();
-            this.ResumeLayout(false);
+        }
 
+        // Initialize all form controls
+        private void InitializeFormControls()
+        {
+            // Order Number
+            this.lblOrderNumber = new Label { Text = "Order Number:", Location = new System.Drawing.Point(20, 30) };
+            this.txtOrderNumber = new TextBox { Location = new System.Drawing.Point(150, 30), Size = new System.Drawing.Size(300, 25) };
+
+            // Name
+            this.lblName = new Label { Text = "Your Name:", Location = new System.Drawing.Point(20, 80) };
+            this.txtName = new TextBox { Location = new System.Drawing.Point(150, 80), Size = new System.Drawing.Size(300, 25) };
+
+            // Email
+            this.lblEmail = new Label { Text = "Email Address:", Location = new System.Drawing.Point(20, 130) };
+            this.txtEmail = new TextBox { Location = new System.Drawing.Point(150, 130), Size = new System.Drawing.Size(300, 25) };
+
+            // Product Rating
+            this.lblProductRating = new Label { Text = "Product Rating (1-5):", Location = new System.Drawing.Point(20, 180) };
+            this.cmbProductRating = new ComboBox { Items = { "1", "2", "3", "4", "5" }, Location = new System.Drawing.Point(150, 180), Size = new System.Drawing.Size(100, 25) };
+
+            // Service Rating
+            this.lblServiceRating = new Label { Text = "Service Rating (1-5):", Location = new System.Drawing.Point(20, 230) };
+            this.cmbServiceRating = new ComboBox { Items = { "1", "2", "3", "4", "5" }, Location = new System.Drawing.Point(150, 230), Size = new System.Drawing.Size(100, 25) };
+
+            // Feedback Type
+            this.lblFeedbackType = new Label { Text = "Feedback Type:", Location = new System.Drawing.Point(20, 280) };
+            this.cmbFeedbackType = new ComboBox { Items = { "Product", "Service", "Both" }, Location = new System.Drawing.Point(150, 280), Size = new System.Drawing.Size(150, 25) };
+
+            // Comments
+            this.lblComments = new Label { Text = "Comments:", Location = new System.Drawing.Point(20, 330) };
+            this.txtComments = new TextBox { Location = new System.Drawing.Point(150, 330), Size = new System.Drawing.Size(600, 80), Multiline = true };
+
+            // Submit Button
+            this.btnSubmit = new Button
+            {
+                Text = "Submit Feedback",
+                Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold),
+                ForeColor = System.Drawing.Color.White,
+                BackColor = System.Drawing.Color.Green,
+                Location = new System.Drawing.Point(150, 430),
+                Size = new System.Drawing.Size(200, 40)
+            };
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+
+            // Reset Button
+            this.btnReset = new Button
+            {
+                Text = "Reset Form",
+                Font = new System.Drawing.Font("Arial", 12F),
+                ForeColor = System.Drawing.Color.White,
+                BackColor = System.Drawing.Color.Gray,
+                Location = new System.Drawing.Point(370, 430),
+                Size = new System.Drawing.Size(150, 40)
+            };
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+
+            // Image Upload
+            this.lblImageUpload = new Label { Text = "Upload Image (Optional):", Location = new System.Drawing.Point(450, 30) };
+            this.btnUploadImage = new Button { Text = "Choose File", Location = new System.Drawing.Point(600, 30), Size = new System.Drawing.Size(100, 30) };
+            this.btnUploadImage.Click += new System.EventHandler(this.btnUploadImage_Click);
+
+            // Image Status
+            this.lblImageStatus = new Label { Location = new System.Drawing.Point(450, 70), Size = new System.Drawing.Size(250, 20), Text = "No image uploaded." };
         }
     }
 }
--- Feedback.cs
+++ Feedback.cs
@@ -12,33 +12,59 @@ namespace Final_Project_for_Studio_2
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
+            string orderNumber = txtOrderNumber.Text;
             string name = txtName.Text;
             string email = txtEmail.Text;
-            string rating = cmbRating.SelectedItem?.ToString();
+            string productRating = cmbProductRating.SelectedItem?.ToString();
+            string serviceRating = cmbServiceRating.SelectedItem?.ToString();
+            string feedbackType = cmbFeedbackType.SelectedItem?.ToString();
             string comments = txtComments.Text;
 
-            // Simple validation
-            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(rating))
+            if (string.IsNullOrEmpty(orderNumber) || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(email) ||
+                string.IsNullOrEmpty(productRating) || string.IsNullOrEmpty(serviceRating))
             {
-                MessageBox.Show("Please fill in all fields before submitting!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Please fill in all required fields before submitting.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
-                // Here you could send feedback to a server or store it in a database
-                MessageBox.Show($"Thank you, {name}, for your feedback!\nRating: {rating}\nComments: {comments}",
+                MessageBox.Show($"Thank you, {name}, for your feedback!\nOrder Number: {orderNumber}\n" +
+                                 $"Product Rating: {productRating}\nService Rating: {serviceRating}\n" +
+                                 $"Feedback Type: {feedbackType}\nComments: {comments}",
                                  "Feedback Submitted", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                // Clear fields after submission
+                // Clear the form after submission
+                txtOrderNumber.Clear();
                 txtName.Clear();
                 txtEmail.Clear();
-                cmbRating.SelectedIndex = -1;
+                cmbProductRating.SelectedIndex = -1;
+                cmbServiceRating.SelectedIndex = -1;
+                cmbFeedbackType.SelectedIndex = -1;
                 txtComments.Clear();
+                lblImageStatus.Text = "No image uploaded.";
             }
         }
 
-        private void lblSubHeader_Click(object sender, EventArgs e)
+        private void btnReset_Click(object sender, EventArgs e)
         {
+            txtOrderNumber.Clear();
+            txtName.Clear();
+            txtEmail.Clear();
+            cmbProductRating.SelectedIndex = -1;
+            cmbServiceRating.SelectedIndex = -1;
+            cmbFeedbackType.SelectedIndex = -1;
+            txtComments.Clear();
+            lblImageStatus.Text = "No image uploaded.";
+        }
 
+        private void btnUploadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                lblImageStatus.Text = $"Image uploaded: {openFileDialog.FileName}";
+            }
         }
     }
 }
