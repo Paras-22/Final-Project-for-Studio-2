c760db876bf2cf97cf058611d96b34f457d1053e
mazhar1341 <faizimazhar2@gmail.com>
2024-11-25 15:11:32 +1300

    added new form for women catagory

---
+++ womencategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project_for_Studio_2
+{
+    public partial class womencategory : Form
+    {
+        public womencategory()
+        {
+            InitializeComponent();
+        }
+    }
+}
