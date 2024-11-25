827e8dbe4075da5362c739e3bebb64a8d3cb02dc
mazhar1341 <faizimazhar2@gmail.com>
2024-11-25 15:16:13 +1300

    created some degine and added some code

--- womencategory.Designer.cs
+++ womencategory.Designer.cs
@@ -1,16 +1,9 @@
 ﻿namespace Final_Project_for_Studio_2
 {
-    partial class womencategory
+    partial class WomenCategory
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +13,78 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.listBoxCategories = new System.Windows.Forms.ListBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(120, 30);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(210, 31);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Women's Category";
+
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(10, 10);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 30);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+
+            // 
+            // listBoxCategories
+            // 
+            this.listBoxCategories.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.listBoxCategories.FormattingEnabled = true;
+            this.listBoxCategories.ItemHeight = 20;
+            this.listBoxCategories.Location = new System.Drawing.Point(50, 80);
+            this.listBoxCategories.Name = "listBoxCategories";
+            this.listBoxCategories.Size = new System.Drawing.Size(350, 124);
+            this.listBoxCategories.TabIndex = 2;
+            this.listBoxCategories.SelectedIndexChanged += new System.EventHandler(this.listBoxCategories_SelectedIndexChanged);
+
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.labelDescription.Location = new System.Drawing.Point(50, 220);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(0, 20);
+            this.labelDescription.TabIndex = 3;
+
+            // 
+            // WomenCategory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "womencategory";
+            this.ClientSize = new System.Drawing.Size(450, 300);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.listBoxCategories);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "WomenCategory";
+            this.Text = "Women Category";
+            this.Load += new System.EventHandler(this.WomenCategory_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.ListBox listBoxCategories;
+        private System.Windows.Forms.Label labelDescription;
     }
 }
